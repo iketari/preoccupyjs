b18ff2e62d0509a842d3633586d9393c5fca48a2
Artsiom Mezin <iketari@gmail.com>
2019-02-20 07:57:20 +0000

    docs(test-spa/index.html): Force the demo SPA to https by js

--- test-spa/src/main.ts
+++ test-spa/src/main.ts
@@ -2,15 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  if (location.protocol !== 'https:') {
-    location.href = location.href.replace('http', 'https');
-  }
-
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
